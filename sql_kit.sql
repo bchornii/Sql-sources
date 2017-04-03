5ea5d78589227d283eba68753e442fadd5ffc187
bchornii <bogdan.chorniy@gmail.com>
2017-04-03 23:00:17 +0300

    Update sql_kit.sql

--- sql_kit.sql
+++ sql_kit.sql
@@ -1,10 +1,6 @@
 ﻿USE TSQLFundamentals2008;
 GO
 
-
--- remote
--- local
-
 --SELECT o.custid, YEAR(o.orderdate) AS order_date
 --FROM TSQLFundamentals2008.Sales.Orders AS o
 --ORDER BY o.custid, YEAR(o.orderdate)
